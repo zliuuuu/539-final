b3ebcb2f4d64d62690939e5f7f4cc62ca5f09664
Zhiyuan Liu <143683511+zliuuuu@users.noreply.github.com>
2023-12-06 21:20:47 -0500

    Delete Final directory

--- Final/click.js
+++
@@ -1,8 +0,0 @@
-function flip(element) {
-    var flipper = element.querySelector(".flipper");
-    if (flipper.style.transform == "rotateY(180deg)") {
-        flipper.style.transform = "rotateY(0deg)";
-    } else {
-        flipper.style.transform = "rotateY(180deg)";
-    }
-}
